6c499e0236258bb0d831675c48bf047ca617c0b2
SSarafina Emmanuely <sarafinaemmanuely05@gmail.com>
2026-07-27 22:25:33 +0400

    Setting up and testing the print statement

---
+++ main.py
@@ -0,0 +1,5 @@
+# Programming One Formative Assignment 2026
+# Done by Sarafina Emmanuely Barnabas - CS 2026
+# This is my first programming assignment for Programming One Course 2026.
+
+print("A student grade/assignment tracker to enable users to record homework and exam results, view and filter assignments and display the grade summaries within a single terminal session.")
