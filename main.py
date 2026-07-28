3bd4fdce7a9d9e254968e28a34266cdd78aff885
SSarafina Emmanuely <sarafinaemmanuely05@gmail.com>
2026-07-28 22:35:06 +0400

    Created the menu list for users

--- main.py
+++ main.py
@@ -3,3 +3,11 @@
 # This is my first programming assignment for Programming One Course 2026.
 
 print("A student grade/assignment tracker to enable users to record homework and exam results, view and filter assignments and display the grade summaries within a single terminal session.")
+#Creating a menu list where users can select from using print statements
+print(" Below is a menu list wwhere you can choose from;")
+print("1. Add homework")
+print("2. Add examination")
+print("3. List assignments")
+print("4. Filter out by subject/type or month ")
+print("5. Show summary")
+print("0. Exit")
