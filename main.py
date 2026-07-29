bbcd405a3648184f3ac3377cb7da1c5f35947ee0
Sarafina Barnabas <s.barnabas@alustudent.com>
2026-07-29 20:07:38 +0400

    I added a function with input in order for users to make their selections.

--- main.py
+++ main.py
@@ -4,10 +4,26 @@
 
 print("A student grade/assignment tracker to enable users to record homework and exam results, view and filter assignments and display the grade summaries within a single terminal session.")
 #Creating a menu list where users can select from using print statements
-print(" Below is a menu list wwhere you can choose from;")
+print(" Below is a menu list where you can choose from;")
 print("1. Add homework")
 print("2. Add examination")
 print("3. List assignments")
-print("4. Filter out by subject/type or month ")
+print("4. Filter out by subject/type or month")
 print("5. Show summary")
 print("0. Exit")
+
+#Creating a function that has a menu and input statements to allow users to make a choice from the menu
+def menu():
+    print(" Below is a menu list where you can choose from;")
+    print("1. Add homework")
+    print("2. Add examination")
+    print("3. List assignments")
+    print("4. Filter out by subject/type or month")
+    print("5. Show summary")
+    print("0. Exit")
+    selection = input("Select an option from the menu list provided:")
+    return selection
+
+#In order for this function to run I have to call the funtion and assign it to a variable so that I can be able to print the menu list and what the user will select.
+user_selection = menu()
+print("You selected option: " + user_selection)
