d88bdb957f616a1a1beb0e66c8aa8751292bac08
sbarnabas-prog <s.barnabas@alustudent.com>
2026-08-03 18:12:44 +0400

    Deleted the duplicate menu option that i used to create the menu print statement

--- main.py
+++ main.py
@@ -3,14 +3,6 @@
 # This is my first programming assignment for Programming One Course 2026.
 
 print("A student grade/assignment tracker to enable users to record homework and exam results, view and filter assignments and display the grade summaries within a single terminal session.")
-#Creating a menu list where users can select from using print statements
-print(" Below is a menu list where you can choose from;")
-print("1. Add homework")
-print("2. Add examination")
-print("3. List assignments")
-print("4. Filter out by subject/type or month")
-print("5. Show summary")
-print("0. Exit")
 
 #Creating a function that has a menu and input statements to allow users to make a choice from the menu
 def menu():
